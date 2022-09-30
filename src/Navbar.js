8f87131ecf04bfe1ff06718b7f4798d7adebd0a0
Neven <95269427+Proclus01@users.noreply.github.com>
2022-09-29 20:54:52 -0700

    install mui icons, add boilerplate to navbar component, link up navbar to app js

---
+++ src/Navbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { AppBar } from "@mui/material";
+import Toolbar from "@mui/material";
+import IconButton from "@mui/material";
+import Typography from "@mui/material";
+import InputBase from "@mui/material";
+import Switch from "@mui/material";
+import { Search } from "@mui/icons-material";
+import withStyles from "@mui/material";
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Navbar is here</h1>
+            </div>
+        )
+    }
+}
+
+export default Navbar;
