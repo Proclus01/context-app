53a7a5fff4bcaabb3bfa0c489679bf8299c7d434
Neven <95269427+Proclus01@users.noreply.github.com>
2022-09-29 22:47:01 -0700

    add mui components to navbar, add basic navbar css

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,40 @@
 import React, { Component } from "react";
 import { AppBar } from "@mui/material";
-import Toolbar from "@mui/material";
-import IconButton from "@mui/material";
-import Typography from "@mui/material";
-import InputBase from "@mui/material";
-import Switch from "@mui/material";
+import {Toolbar} from "@mui/material";
+import {IconButton} from "@mui/material";
+import {Typography} from "@mui/material";
+import {InputBase} from "@mui/material";
+import {Switch} from "@mui/material";
 import { Search } from "@mui/icons-material";
-import withStyles from "@mui/material";
+import {withStyles} from "@mui/material";
+import { ClassNames } from "@emotion/react";
+import './Navbar.css';
 
 class Navbar extends Component {
     render() {
         return (
-            <div>
-                <h1>Navbar is here</h1>
+            <div className="root">
+                <AppBar position="static" color="primary">
+                    <Toolbar>
+                        <IconButton className="menuButton" color="inherit">
+                            <span>flag</span>
+                        </IconButton>
+                        <Typography
+                            variant="h6"
+                            color="inherit"
+                            className="title"
+                        >
+                            App Title
+                        </Typography>
+                        <Switch />
+                        <div className="grow"/>
+                        <div className="search">
+                            <div className="searchIcon">
+                                <Search />
+                            </div>
+                        </div>
+                    </Toolbar>
+                </AppBar>
             </div>
         )
     }
